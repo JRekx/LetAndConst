a57120130f8553d900b1f492c7fe3eb2b75e33ed
JRekx <131090763+JRekx@users.noreply.github.com>
2023-05-27 23:21:33 -0700

    Add files via upload

---
+++ Let and const Excerise/app.js
@@ -0,0 +1,20 @@
+var PI = 3.14
+PI = 42
+//gets turned into
+
+let PI = 3.14
+
+//OR 
+
+const PI = 3.14
+
+// The diffrence between let and var is that you can redclare var but can't redeclare let.
+// You can also access a hoisted varable with var and let creates block scope.
+
+// The diffrence bettween var and const is the same with as of let.
+
+// The diffrence betwen let is const is that let can be reassigned but you cant redeclare const.
+
+//Hoisting is the way Js complies code. Variables are hoisted to the top of the scope they are declared in.
+// var can acces a varrables name and its undefined value before its used late on.
+
